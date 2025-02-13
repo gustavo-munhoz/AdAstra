0c85a73b5d26aac4eaeb3eae0b144af35efaff6f
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-12 22:18:26 -0300

    Reorganized and created new files

---
+++ AdAstra/Scenes/SolarSystem/Model/SolarSystem.swift
@@ -0,0 +1,8 @@
+//
+//  SolarSystem.swift
+//  AdAstra
+//
+//  Created by Gustavo Munhoz Correa on 12/02/25.
+//
+
+import Foundation
---
+++ AdAstra/Scenes/UserPlanet/ViewModel/UserPlanetViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserPlanetViewModel.swift
+//  AdAstra
+//
+//  Created by Gustavo Munhoz Correa on 12/02/25.
+//
+
+import Foundation
