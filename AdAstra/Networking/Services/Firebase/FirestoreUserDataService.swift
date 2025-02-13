e0ab6c4a8cd1655496b3c1371ecf2f67611b28ed
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-13 16:39:55 -0300

    Implemented Firestore service

--- AdAstra/Networking/Services/Firebase/FirestoreUserDataService.swift
+++ AdAstra/Networking/Services/Firebase/FirestoreUserDataService.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+enum FirestoreCollection: String {
+    case users
+}
+
+class FirestoreUserDataService: UserDataService {
+    
+    private let db = Firestore.firestore()
+    
+    func saveUser(_ user: FirebaseUserDTO) async throws {
+        try db
+            .collection(FirestoreCollection.users.rawValue)
+            .document(user.uid)
+            .setData(from: user)
+    }
+    
+    func fetchUser(withUserID uid: String) async throws -> FirebaseUserDTO {
+        try await db
+            .collection(FirestoreCollection.users.rawValue)
+            .document(uid)
+            .getDocument(as: FirebaseUserDTO.self)
+    }
+    
+    func fetchAllUsers() async throws -> [FirebaseUserDTO] {
+        try await db
+            .collection(FirestoreCollection.users.rawValue)
+            .getDocuments()
+            .documents
+            .compactMap { document in
+                try document.data(as: FirebaseUserDTO.self)
+            }
+    }
+}
