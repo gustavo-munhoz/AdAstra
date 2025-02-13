6f55716f6ccae1e25ad745e8b48defbc6fba9974
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-13 16:39:01 -0300

    Refactored `UserDataService` a bit

--- AdAstra/Networking/Services/Protocols/UserDataService.swift
+++ AdAstra/Networking/Services/Protocols/UserDataService.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+protocol UserDataService {
+    func saveUser(_ user: FirebaseUserDTO) async throws
+    func fetchUser(withUserID uid: String) async throws -> FirebaseUserDTO
+    func fetchAllUsers() async throws -> [FirebaseUserDTO]
+}
