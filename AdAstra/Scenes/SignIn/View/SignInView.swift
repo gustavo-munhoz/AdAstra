22126a48284faffacf34b1913c452c1f27fe0775
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-13 23:09:44 -0300

    Created new files

---
+++ AdAstra/Scenes/SignIn/View/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  AdAstra
+//
+//  Created by Gustavo Munhoz Correa on 13/02/25.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
---
+++ AdAstra/Scenes/SignIn/ViewModel/SignInViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignInViewModel.swift
+//  AdAstra
+//
+//  Created by Gustavo Munhoz Correa on 13/02/25.
+//
+
+import Foundation
