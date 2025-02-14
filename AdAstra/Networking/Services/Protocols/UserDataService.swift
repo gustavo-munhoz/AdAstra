a5fd64641398a432b96392b01029c4bc6f0e9fbe
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-13 23:11:46 -0300

    Renamed user dto struct removing firebase from name

--- AdAstra/Networking/Services/Protocols/UserDataService.swift
+++ AdAstra/Networking/Services/Protocols/UserDataService.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol UserDataService {
-    func saveUser(_ user: FirebaseUserDTO) async throws
-    func fetchUser(withUserID uid: String) async throws -> FirebaseUserDTO
-    func fetchAllUsers() async throws -> [FirebaseUserDTO]
+    func saveUser(_ user: UserDTO) async throws
+    func fetchUser(withUserID uid: String) async throws -> UserDTO
+    func fetchAllUsers() async throws -> [UserDTO]
 }
