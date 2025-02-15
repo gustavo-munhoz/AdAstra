91f92d7319d0bfdce6a9f288bd20d76f44280f74
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-14 23:53:14 -0300

    Implemented view model that loads users in init

--- AdAstra/Scenes/UsersGrid/ViewModel/UsersGridViewModel.swift
+++ AdAstra/Scenes/UsersGrid/ViewModel/UsersGridViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class UsersGridViewModel: ObservableObject {
+    
+    @Published var users: [User] = []
+    @Published var isFetchingUsers: Bool = false
+    
+    init() {
+        let facade = UserPersistenceFacade.firebase()
+        
+        Task {
+            await MainActor.run { isFetchingUsers = true }
+            
+            let fetchedUsers = await facade.getAllUsers()
+            
+            await MainActor.run {
+                users = fetchedUsers
+            }
+            
+            await MainActor.run { isFetchingUsers = false }
+        }
+    }
+}
