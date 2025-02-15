290a61a442bce33a582ad4ae585a1c3252526d4c
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-14 23:52:59 -0300

    Created basic structure for users grid view

--- AdAstra/Scenes/UsersGrid/View/UsersGridView.swift
+++ AdAstra/Scenes/UsersGrid/View/UsersGridView.swift
@@ -5,4 +5,45 @@
 //  Created by Gustavo Munhoz Correa on 14/02/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct UsersGridView: View {
+    
+    @StateObject private var viewModel = UsersGridViewModel()
+    
+    private let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        VStack {
+            Group {
+                if viewModel.isFetchingUsers {
+                    ProgressView()
+                    
+                } else {
+                    LazyVGrid(columns: columns, spacing: 12) {
+                        ForEach(viewModel.users) { user in
+                            NavigationLink(destination: { UserPlanetView() }) {
+                                VStack {
+                                    Circle()
+                                    
+                                    Text(user.name)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .navigationBarBackButtonHidden()
+        .padding()
+    }
+}
+
+#Preview {
+    UsersGridView()
+}
