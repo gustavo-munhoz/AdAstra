6cc685f64aee98844a2c2bb438d04fe690a58a38
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-18 18:02:43 -0300

    Created method to map planet dto to planet

--- AdAstra/Networking/Models/PlanetDTO.swift
+++ AdAstra/Networking/Models/PlanetDTO.swift
@@ -7,12 +7,40 @@
 
 import Foundation
 
+enum PlanetMappingError: Error {
+    case invalidGradientName
+    case invalidTextureName
+}
+
 struct PlanetDTO: Codable {
+    // MARK: - Attributes
     let name: String
+    let gradientName: String
+    let textureName: String
+    
+    // MARK: - Mapping Methods
+    
+    func mappedToPlanet() throws -> Planet {
+        guard let gradientName = GradientName.fromString(gradientName) else {
+            throw PlanetMappingError.invalidGradientName
+        }
+        
+        guard let textureName = TextureName.fromString(textureName) else {
+            throw PlanetMappingError.invalidTextureName
+        }
+        
+        return Planet(
+            name: name,
+            gradientName: gradientName,
+            textureName: textureName
+        )
+    }
     
     static func mappedFrom(planet: Planet) -> PlanetDTO {
         PlanetDTO(
-            name: planet.name
+            name: planet.name,
+            gradientName: planet.gradientName.rawValue,
+            textureName: planet.textureName.rawValue
         )
     }
 }
