a853fbe0eb17019e53c02f04d8e7f9190685a4d5
Gustavo Isoo Binder <gustan.binder@gmail.com>
2025-02-18 08:52:15 -0300

    SceneKit views for planet selection screen

--- AdAstra/Scenes/UserPlanet/View/ScrollSelectorView.swift
+++ AdAstra/Scenes/UserPlanet/View/ScrollSelectorView.swift
@@ -6,29 +6,35 @@
 //
 
 import SwiftUI
+import SceneKit
 
 struct ScrollSelectorView: View {
     let scaleFactor = 0.45
     let offsetValue = -40
-    @Binding var value: Int
-
+    var value: Binding<Int>
+    
+    var planetViews : [TDPlanetView]
+    
+    init(value: Binding<Int>) {
+        self.value = value
+        self.planetViews = []
+        
+        for _ in 0..<10 {
+            planetViews.append(TDPlanetView())
+        }
+    }
+    
     var body: some View {
         HStack {
             ScrollView(.vertical) {
                 LazyVStack {
-                    ForEach(0..<10, id: \.self) { i in
+                    ForEach(0..<10, id:\.self) { i in
                         ZStack {
-                            Circle()
-                                .fill(Color(hue: Double(i) / 10, saturation: 1, brightness: 1).gradient)
+                            planetViews[i]
                                 .frame(height: 150)
                                 .scaleEffect(scaleCalc(index: i))
                                 .offset(x: offsetCalc(index:i))
-                                .animation(.snappy, value: value)
-                            
-                            Text("\(i)")
-                                .scaleEffect(scaleCalc(index: i))
-                                .offset(x: offsetCalc(index:i))
-                                .animation(.snappy, value: value)
+                                .animation(.snappy, value: value.wrappedValue)
                         }
                     }
                 }
@@ -36,10 +42,10 @@ struct ScrollSelectorView: View {
             }
             .scrollTargetBehavior(.viewAligned)
             .scrollPosition(id: .init(get: {
-                let position: Int = value
+                let position: Int = value.wrappedValue
                 return position
             }, set: { newValue in
-                if let newValue { value = newValue }
+                if let newValue { value.wrappedValue = newValue }
             }))
             .safeAreaPadding(150)
             .overlay(alignment: .center, content: {
@@ -52,13 +58,18 @@ struct ScrollSelectorView: View {
     }
     
     func scaleCalc(index: Int) -> Double {
-        let distance = abs(Double(index) - Double(value))
-        return 1.0 - (distance * scaleFactor) // Ensures it gets smaller the further it is
+        let distance = abs(Double(index) - Double(value.wrappedValue))
+        var scaleFinal = 1.0 - (distance * scaleFactor)
+        if scaleFinal < 0.35 {
+            scaleFinal = 0.35
+        }
+        return scaleFinal // Ensures it gets smaller the further it is
     }
     
     func offsetCalc(index: Int) -> Double {
-        let distance = abs(Double(index) - Double(value))
-        return -(1.0 - (distance * Double(offsetValue))) // Ensures it gets smaller the further it is
+        let distance = abs(Double(index) - Double(value.wrappedValue))
+//        return -(1.0 - (distance * Double(offsetValue))) // Ensures it gets smaller the further it is
+        return distance * Double(offsetValue)
     }
 }
 
