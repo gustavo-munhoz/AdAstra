ed5190c648b88ca3709b3381618a448ff26d1d3e
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-18 14:19:07 -0300

    Added methods to facilitate user connections

--- AdAstra/Scenes/UserPlanet/Model/User.swift
+++ AdAstra/Scenes/UserPlanet/Model/User.swift
@@ -7,7 +7,12 @@
 
 import UIKit
 
-struct User: Identifiable {
+struct UserSummary: Codable {
+    let name: String
+    let connectionPassword: String
+}
+
+struct User: Identifiable, Equatable {
     let id: String
     let name: String
     let course: String
@@ -17,9 +22,52 @@ struct User: Identifiable {
     let pronouns: String
     let connectionPassword: String
     let connectionCount: Int
-    let connectedUsers: [User]
+    let connectedUsers: [UserSummary]
     let secretFact: String
-
+    
     let profilePicture: UIImage
     let planet: Planet
+    
+    static func == (lhs: User, rhs: User) -> Bool {
+        lhs.id == rhs.id
+        && lhs.name == rhs.name
+        && lhs.connectionPassword == rhs.connectionPassword
+    }
+    
+    func summary() -> UserSummary {
+        UserSummary(
+            name: name,
+            connectionPassword: connectionPassword
+        )
+    }
+    
+    func canConnect(to user: User) -> Bool {
+        guard user != self else { return false }
+        
+        return !connectedUsers.contains(where: {
+            $0.name == user.name
+            && $0.connectionPassword == user.connectionPassword
+        })
+    }
+    
+    func addingConnection(with user: User) -> User {
+        let newConnectionCount = connectionCount + 1
+        let newConnectedUsers = connectedUsers + [user.summary()]
+        
+        return User(
+            id: id,
+            name: name,
+            course: course,
+            institution: institution,
+            shift: shift,
+            interests: interests,
+            pronouns: pronouns,
+            connectionPassword: connectionPassword,
+            connectionCount: newConnectionCount,
+            connectedUsers: newConnectedUsers,
+            secretFact: secretFact,
+            profilePicture: profilePicture,
+            planet: planet
+        )
+    }
 }
