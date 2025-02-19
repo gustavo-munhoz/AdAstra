f1de9fce59ecbe58e4333237f087baa0430ed1d4
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-19 20:48:10 -0300

    Renamed file and implemented user filters

--- AdAstra/Scenes/UsersGrid/ViewModel/UsersListViewModel.swift
+++ AdAstra/Scenes/UsersGrid/ViewModel/UsersListViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  UsersGridViewModel.swift
+//  UsersListViewModel.swift
 //  AdAstra
 //
 //  Created by Gustavo Munhoz Correa on 14/02/25.
@@ -8,10 +8,26 @@
 import Foundation
 import UIKit
 
-class UsersGridViewModel: ObservableObject {
+class UsersListViewModel: ObservableObject {
     
     @Published var users: [User] = []
     @Published var isFetchingUsers: Bool = false
+    
+    var morningStudents: [User] {
+        users
+            .filterByShifts(.morning)
+            .filterByRoles(.student)
+    }
+    
+    var afternoonStudents: [User] {
+        users
+            .filterByShifts(.afternoon)
+            .filterByRoles(.student)
+    }
+    
+    var mentors: [User] {
+        users.filterByRoles(.mentor, .mentorJr)
+    }
     
     init(mock: Bool = false) {
         if mock {
