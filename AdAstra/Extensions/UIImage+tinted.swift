4d43a7041da13236cb144a73c7510b80df257f5e
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-21 01:48:01 -0300

    Moved UIImage extension to its own file

--- AdAstra/Extensions/UIImage+tinted.swift
+++ AdAstra/Extensions/UIImage+tinted.swift
@@ -5,4 +5,19 @@
 //  Created by Gustavo Munhoz Correa on 21/02/25.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+    func tinted(with color: UIColor) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: self.size)
+        return renderer.image { context in
+            let rect = CGRect(origin: .zero, size: self.size)
+            
+            self.draw(in: rect)
+            
+            context.cgContext.setBlendMode(.multiply)
+            context.cgContext.setFillColor(color.cgColor)
+            context.cgContext.fill(rect)
+        }
+    }
+}
