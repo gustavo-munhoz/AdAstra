675e5e76b1bb47abea6fcc32efcd99a0e0e25f1c
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-21 01:47:47 -0300

    Implemented cache classes for planet optimization

--- AdAstra/Cache/ImageCache.swift
+++ AdAstra/Cache/ImageCache.swift
@@ -5,4 +5,22 @@
 //  Created by Gustavo Munhoz Correa on 21/02/25.
 //
 
-import Foundation
+import UIKit
+
+class ImageCache {
+    static var cache: [String: UIImage] = [:]
+    
+    static func key(for texture: TextureName, gradient: GradientName) -> String {
+        return "\(texture.rawValue)_\(gradient.rawValue)"
+    }
+    
+    static func image(for texture: TextureName, gradient: GradientName) -> UIImage? {
+        let key = self.key(for: texture, gradient: gradient)
+        return cache[key]
+    }
+    
+    static func store(image: UIImage, for texture: TextureName, gradient: GradientName) {
+        let key = self.key(for: texture, gradient: gradient)
+        cache[key] = image
+    }
+}
--- AdAstra/Cache/SceneCache.swift
+++ AdAstra/Cache/SceneCache.swift
@@ -5,4 +5,18 @@
 //  Created by Gustavo Munhoz Correa on 21/02/25.
 //
 
-import Foundation
+import SceneKit
+
+class SceneCache {
+    static var cache: [String: SCNScene] = [:]
+    
+    static func scene(for texture: TextureName, gradient: GradientName) -> SCNScene? {
+        let key = "\(texture.rawValue)_\(gradient.rawValue)"
+        return cache[key]
+    }
+    
+    static func store(scene: SCNScene, for texture: TextureName, gradient: GradientName) {
+        let key = "\(texture.rawValue)_\(gradient.rawValue)"
+        cache[key] = scene
+    }
+}
