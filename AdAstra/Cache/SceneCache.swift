b06d1e7698f4ca7933d56f166eed27416806ef8d
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-21 01:47:32 -0300

    Added new files

---
+++ AdAstra/Cache/SceneCache.swift
@@ -0,0 +1,8 @@
+//
+//  SceneCache.swift
+//  AdAstra
+//
+//  Created by Gustavo Munhoz Correa on 21/02/25.
+//
+
+import Foundation
---
+++ AdAstra/Extensions/UIImage+tinted.swift
@@ -0,0 +1,8 @@
+//
+//  UIImage+tinted.swift
+//  AdAstra
+//
+//  Created by Gustavo Munhoz Correa on 21/02/25.
+//
+
+import Foundation
