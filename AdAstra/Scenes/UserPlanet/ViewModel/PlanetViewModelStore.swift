1dd7a54c522cb86a1d6babd1f0e76bb49a018c03
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-21 18:30:08 -0300

    Created view model and store to optimize planet creation

--- AdAstra/Scenes/UserPlanet/ViewModel/PlanetViewModelStore.swift
+++ AdAstra/Scenes/UserPlanet/ViewModel/PlanetViewModelStore.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+/// This class is used to avoid creating multiple instances of scenes.
+/// For example, a scene could be reused for both `UsersGridView` and `ScrollSelectorView`, if the user is the same.
+class PlanetViewModelStore: ObservableObject {
+    private(set) var models: [String: PlanetViewModel] = [:]
+
+    func model(for user: User) -> PlanetViewModel {
+        let key = "\(user.planet.textureName.rawValue)_\(user.planet.gradientName.rawValue)"
+        
+        if let existing = models[key] {
+            return existing
+        } else {
+            let newModel = PlanetViewModel(user: user)
+            models[key] = newModel
+
+            return newModel
+        }
+    }
+}
