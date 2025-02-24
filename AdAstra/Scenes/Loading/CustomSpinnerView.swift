8b8b4ffb33ffa44ddd332dcb4dbef61bc63d33e0
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-23 22:13:10 -0300

    Created new files

---
+++ AdAstra/Scenes/Loading/CustomSpinnerView.swift
@@ -0,0 +1,8 @@
+//
+//  CustomSpinnerView.swift
+//  AdAstra
+//
+//  Created by Gustavo Munhoz Correa on 22/02/25.
+//
+
+import Foundation
---
+++ AdAstra/Scenes/Loading/LoadingView.swift
@@ -0,0 +1,8 @@
+//
+//  LoadingView.swift
+//  AdAstra
+//
+//  Created by Gustavo Munhoz Correa on 22/02/25.
+//
+
+import Foundation
