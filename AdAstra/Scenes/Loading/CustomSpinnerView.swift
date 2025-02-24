197dcafd0c7cd6359bc166a01270ecc59e539ca3
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-23 22:16:30 -0300

    Implemented new loading screen and custom spinner

--- AdAstra/Scenes/Loading/CustomSpinnerView.swift
+++ AdAstra/Scenes/Loading/CustomSpinnerView.swift
@@ -5,4 +5,42 @@
 //  Created by Gustavo Munhoz Correa on 22/02/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct CustomSpinnerView: View {
+    var loadingText: String? = nil
+
+    @State private var startTrim: CGFloat = 0.0
+    @State private var endTrim: CGFloat = 0.1
+    @State private var rotation: Double = 0
+
+    var body: some View {
+        VStack(spacing: 12) {
+            Circle()
+                .trim(from: startTrim, to: endTrim)
+                .stroke(.gradientWhite1, style: StrokeStyle(lineWidth: 4, lineCap: .round))
+                .frame(width: 28, height: 28)
+                .rotationEffect(.degrees(rotation))
+                .onAppear {
+                    withAnimation(Animation.linear(duration: 1.5).repeatForever(autoreverses: false)) {
+                        rotation = 360
+                    }
+                    withAnimation(Animation.easeOut(duration: 1.0).repeatForever(autoreverses: true)) {
+                        endTrim = 0.75
+                    }
+                    withAnimation(Animation.easeIn(duration: 1.0).repeatForever(autoreverses: true)) {
+                        startTrim = 0.1
+                    }
+                }
+                        
+            if let loadingText {
+                Text(loadingText)
+                    .font(.caption)
+                    .fontWeight(.bold)
+                    .foregroundColor(.gradientWhite1)
+                    .fontWidth(.expanded)
+            }
+        }
+    }
+}
+ 
--- AdAstra/Scenes/Loading/LoadingView.swift
+++ AdAstra/Scenes/Loading/LoadingView.swift
@@ -5,4 +5,31 @@
 //  Created by Gustavo Munhoz Correa on 22/02/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct LoadingView: View {
+    var loadingText: String?
+    
+    var body: some View {
+        ZStack {
+            VStack(spacing: 20) {
+                
+                LogoView()
+                    .scaleEffect(CGSize(width: 0.5, height: 0.5))
+                    .padding(.vertical)
+                
+                CustomSpinnerView(loadingText: loadingText)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background {
+            Image("bg")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                .aspectRatio(contentMode: .fill)
+        }
+        .transition(.opacity)
+        .animation(.easeInOut, value: UUID())
+    }
+}
+
