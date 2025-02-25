3ca8121e3ff1cf5ef0843ddf80456600fb4e5375
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-25 02:19:26 -0300

    Implemented parallax background image

--- AdAstra/Scenes/_SharedComponents/Parallax/ParallaxBackground.swift
+++ AdAstra/Scenes/_SharedComponents/Parallax/ParallaxBackground.swift
@@ -5,4 +5,34 @@
 //  Created by Gustavo Munhoz Correa on 25/02/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct ParallaxBackground: View {
+    @StateObject private var motionManager = MotionManager()
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let maxOffset: CGFloat = 15
+            
+            let xOffset = CGFloat(motionManager.roll) * maxOffset
+            let yOffset = CGFloat(motionManager.pitch) * maxOffset
+            
+            Image(.bg)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea(.all)
+                .offset(x: xOffset, y: yOffset)
+                .frame(
+                    width: geometry.size.width + maxOffset * 2,
+                    height: geometry.size.height + maxOffset * 2
+                )
+                .zIndex(2)
+        }
+        .background {
+            Image(.bg)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea(.all)
+        }
+    }
+}
