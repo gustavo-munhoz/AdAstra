ac7e693d7878e83bee5b44ba7e4dcca91928e471
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-02-25 02:19:14 -0300

    Created new files

---
+++ AdAstra/Scenes/_SharedComponents/Parallax/MotionManager.swift
@@ -0,0 +1,8 @@
+//
+//  MotionManager.swift
+//  AdAstra
+//
+//  Created by Gustavo Munhoz Correa on 25/02/25.
+//
+
+import Foundation
---
+++ AdAstra/Scenes/_SharedComponents/Parallax/ParallaxBackground.swift
@@ -0,0 +1,8 @@
+//
+//  ParallaxBackground.swift
+//  AdAstra
+//
+//  Created by Gustavo Munhoz Correa on 25/02/25.
+//
+
+import Foundation
